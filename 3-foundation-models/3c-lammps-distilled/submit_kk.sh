fe4ce728bfcc12090b99184a6273d15a5757eae2
Yury Lysogorskiy <yury.lysogorskiy@rub.de>
2026-02-24 12:26:57 +0100

    b-distillation/seed/1/saved_model + LAMMPS 3c-..

--- 3-foundation-models/3c-lammps-distilled/submit_kk.sh
+++ 3-foundation-models/3c-lammps-distilled/submit_kk.sh
@@ -4,9 +4,9 @@
 #SBATCH -n 1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu 32GB
-#SBATCH -J "HEA25-FS-lammps"
-#SBATCH -p normal
-
+#SBATCH -J "HEA25-kk"
+#SBATCH -p gpu
+#SBATCH --gres=gpu:a100:1
 
 source /pc2/users/u/usrtr200/load_GRACE.sh 
 
@@ -15,5 +15,4 @@ module load system/CUDA/13.0.0 mpi/OpenMPI/5.0.8-GCC-14.3.0
 # Optional: confirm GPU is visible
 
 # Run your application here.
-#/pc2/users/u/usrtr200/mlpfits-2026-grace/soft/lammps/build/lmp -in in.lammps
-/pc2/groups/hpc-prf-mlpfits/GRACE/bin/lmp -in in.lammps
+/pc2/groups/hpc-prf-mlpfits/GRACE/bin/lmp_kk -k on g 1 -sf kk -pk kokkos newton on neigh half -in in.lammps 
