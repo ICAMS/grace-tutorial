6e70f9d0970ef90d99066d4775b32d2ecf662ebf
Yury Lysogorskiy <yury.lysogorskiy@rub.de>
2026-02-27 14:04:35 +0100

    hea25 lammps

--- 2-HEA25S-GRACE-FS/3-lammps/grace-fs-with-extrapolation-grade/submit.sh
+++ 2-HEA25S-GRACE-FS/3-lammps/grace-fs-with-extrapolation-grade/submit.sh
@@ -6,6 +6,7 @@
 #SBATCH --mem-per-cpu 32GB
 #SBATCH -J "HEA25-FS-lammps"
 #SBATCH -p normal
+#SBATCH -A hpc-prf-mlpfits
 
 
 source /pc2/users/u/usrtr200/load_GRACE.sh 
@@ -15,5 +16,4 @@ module load system/CUDA/13.0.0 mpi/OpenMPI/5.0.8-GCC-14.3.0
 # Optional: confirm GPU is visible
 
 # Run your application here.
-#/pc2/users/u/usrtr200/mlpfits-2026-grace/soft/lammps/build/lmp -in in.lammps
 /pc2/groups/hpc-prf-mlpfits/GRACE/bin/lmp -in in.lammps
--- 2-HEA25S-GRACE-FS/3-lammps/grace-fs-with-extrapolation-grade/submit_kk.sh
+++ 2-HEA25S-GRACE-FS/3-lammps/grace-fs-with-extrapolation-grade/submit_kk.sh
@@ -7,13 +7,20 @@
 #SBATCH -J "HEA25-kk"
 #SBATCH -p gpu
 #SBATCH --gres=gpu:a100:1
+#SBATCH -A hpc-prf-mlpfits
+
 
 source /pc2/users/u/usrtr200/load_GRACE.sh 
 
 module load system/CUDA/13.0.0 mpi/OpenMPI/5.0.8-GCC-14.3.0
 
+# ==========================================
+# 3. Job Execution
+# ==========================================
 # Optional: confirm GPU is visible
+echo "CUDA devices: $CUDA_VISIBLE_DEVICES"
+nvidia-smi
 
 # Run your application here.
 #/pc2/users/u/usrtr200/mlpfits-2026-grace/soft/lammps/build/lmp -in in.lammps
-/pc2/groups/hpc-prf-mlpfits/GRACE/bin/lmp_kk -k on g 1 -sf kk -pk kokkos newton on neigh half -in in.lammps 
+/pc2/groups/hpc-prf-mlpfits/GRACE/bin/lmp_kk -k on g 1 -sf kk -pk kokkos newton on neigh half -in in.kk.lammps 
