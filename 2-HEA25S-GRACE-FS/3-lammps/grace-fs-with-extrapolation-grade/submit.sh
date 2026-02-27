49601121b3650304297499aac0963fdbaa87c8fc
Yury Lysogorskiy <yury.lysogorskiy@rub.de>
2026-02-27 14:12:04 +0100

    upd submit.sh

--- 2-HEA25S-GRACE-FS/3-lammps/grace-fs-with-extrapolation-grade/submit.sh
+++ 2-HEA25S-GRACE-FS/3-lammps/grace-fs-with-extrapolation-grade/submit.sh
@@ -2,16 +2,17 @@
 #SBATCH -t 0:15:00
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu 32GB
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
 #SBATCH -J "HEA25-FS-lammps"
 #SBATCH -p normal
 #SBATCH -A hpc-prf-mlpfits
 
-
 source /pc2/users/u/usrtr200/load_GRACE.sh 
 
-module load system/CUDA/13.0.0 mpi/OpenMPI/5.0.8-GCC-14.3.0
+
+# Add the GCC module before OpenMPI
+module load compiler/GCC/14.3.0 system/CUDA/13.0.0 mpi/OpenMPI/5.0.8-GCC-14.3.0
 
 # Optional: confirm GPU is visible
 
